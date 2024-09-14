46426a3d37a819c658f4a6711c20600a3c33534b
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-09-14 15:30:06 +0300

    Views

---
+++ MVC Assm3/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Assm3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUSs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy() 
+        { 
+            return View();
+        }
+    }
+}
